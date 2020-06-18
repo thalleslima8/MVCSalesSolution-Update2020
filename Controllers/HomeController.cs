e3dbebae8fab8a9372eae6172b14a3d9f77afa3a
Thalles Veloso <thalleslima8@gmail.com>
2020-06-18 08:20:12 -0300

    Project Teste add

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace MVCSalesSolution.Controllers
             return View();
         }
 
+        public IActionResult Teste()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
