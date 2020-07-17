8b33eb441b48a187915dd56e2522b1fa59c5eb33
Thalles Veloso <thalleslima8@gmail.com>
2020-07-17 19:49:41 -0300

    Simple create form

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -22,6 +22,10 @@ namespace MVCSalesSolution.Services
             return _context.Seller.ToList();
         }
 
-
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
