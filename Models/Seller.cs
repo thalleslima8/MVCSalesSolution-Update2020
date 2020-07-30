a7e2cb20706dc52e8b82232961ce93a7324b1678
Thalles Veloso <thalleslima8@gmail.com>
2020-07-30 19:50:59 -0300

    Add validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,18 +9,26 @@ namespace MVCSalesSolution.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="{0} Required")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage ="{0} size must be between {2} and {1}")]
         public string Name { get; set; }
 
         [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Enter a valid e-amil")]
+        [Required(ErrorMessage = "{0} Required")]
         public string Email { get; set; }
 
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} Required")]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Required(ErrorMessage = "{0} Required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
